09f4aef091c854b310189c068a3eed17f39f9686
CoderMuffin <john@mooseandlemur.com>
2020-07-13 08:20:27 +0100

    Forked project

---
+++ Assets/JoyConRemote.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+public class JoyConRemote: MonoBehaviour
+{
+    private List<Joycon> joycons;
+
+    // Values made available via Unity
+    public float[] stick;
+    public Vector3 gyro;
+    public Vector3 accel;
+    public int jc_ind = 0;
+    public Quaternion orientation;
+    NetworkStream stream;
+    //SERVER
+    #region private members 	
+    /// <summary> 	
+    /// TCPListener to listen for incomming TCP connection 	
+    /// requests. 	
+    /// </summary> 	
+    private TcpListener tcpListener;
+    /// <summary> 
+    /// Background thread for TcpServer workload. 	
+    /// </summary> 	
+    private Thread tcpListenerThread;
+    /// <summary> 	
+    /// Create handle to connected tcp client. 	
+    /// </summary> 	
+    private TcpClient connectedTcpClient;
+    bool canUpdate = false;
+    #endregion
+
+    // Use this for initialization
+    void Start()
+    {
+        // Start TcpServer background thread 		
+        tcpListenerThread = new Thread(new ThreadStart(ListenForIncommingRequests));
+        tcpListenerThread.IsBackground = true;
+        tcpListenerThread.Start();
+        //tcpListenerThread.Start();
+        gyro = new Vector3(0, 0, 0);
+        accel = new Vector3(0, 0, 0);
+        // get the public Joycon array attached to the JoyconManager in scene
+        joycons = JoyconManager.Instance.j;
+        if (joycons.Count < jc_ind + 1)
+        {
+            //Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (joycons.Count > 0)
+            orientation = joycons[jc_ind].GetVector();
+        else
+            orientation = transform.rotation;
+        //orientation *= Quaternion.AngleAxis(90, new Vector3(1, 0, 0));
+        transform.rotation = orientation;
+        if (connectedTcpClient != null&&!canUpdate)
+        {
+            Debug.Log("<Socket> Connected!");
+            stream = connectedTcpClient.GetStream();
+            canUpdate = true;
+        }
+        if (canUpdate)
+        {
+            SendMessage();
+        }
+    }
+
+    /// <summary> 	
+    /// Runs in background TcpServerThread; Handles incomming TcpClient requests 	
+    /// </summary> 	
+    private void ListenForIncommingRequests()
+    {
+        try
+        {
+
+            Debug.Log("<Socket> Initializing...");
+            // Create listener on localhost port 8052. 			
+            //try { tcpListener.Stop(); } catch { return; }
+            tcpListener = new TcpListener(IPAddress.Any, 4294);
+            tcpListener.Start();
+            
+            Debug.Log("<Socket> Port listener active");
+            Byte[] bytes = new Byte[1024];
+            while (true)
+            {
+                using (connectedTcpClient = tcpListener.AcceptTcpClient())
+                {
+                    // Get a stream object for reading 					
+                    using (NetworkStream lstream = connectedTcpClient.GetStream())
+                    {
+                        int length;
+                        // Read incomming stream into byte arrary. 
+                        while ((length = lstream.Read(bytes, 0, bytes.Length)) != 0)
+                        {
+                            var incommingData = new byte[length];
+                            Array.Copy(bytes, 0, incommingData, 0, length);
+                            // Convert byte array to string message.
+                            Debug.Log("Message Recieved");
+                            string clientMessage = Encoding.ASCII.GetString(incommingData);
+                        }
+                    }
+                }
+            }
+        }
+        catch (SocketException socketException)
+        {
+            Debug.LogError("<Socket> Exception: " + socketException.ToString());
+            tcpListener.Stop();
+        }
+    }
+    /// <summary> 	
+    /// Send message to client using socket connection. 	
+    /// </summary> 	
+    private void SendMessage()
+    {
+        if (connectedTcpClient == null)
+        {
+            return;
+        }
+
+        try
+        {
+            // Get a stream object for writing. 			
+            
+            if (stream.CanWrite)
+            {
+                //Debug.Log("w" + orientation.w);
+                //Debug.Log("x" + orientation.x);
+                //Debug.Log("y" + orientation.y);
+                //Debug.Log("z" + orientation.z);
+                string serverMessage = orientation.w.ToString() + "," + orientation.x.ToString() + "," + orientation.y.ToString() + "," + orientation.z.ToString();
+                // Convert string message to byte array.
+                Debug.Log(serverMessage);
+                byte[] serverMessageAsByteArray = Encoding.ASCII.GetBytes(serverMessage);
+                // Write byte array to socketConnection stream.               
+                stream.Write(serverMessageAsByteArray, 0, serverMessageAsByteArray.Length);
+            }
+        }
+        catch (SocketException socketException)
+        {
+            Debug.Log("Socket exception: " + socketException);
+            tcpListener.Stop();
+        }
+    }
+    public void OnApplicationQuit()
+    {
+        tcpListener.Stop();
+    }
+}
